2138e17c589033d10f051a06de32a0dc27194892
r0995764 <r0995764@student.vives.be>
2024-02-22 19:06:04 +0100

    final

--- Labo 14 - Sem 2/Opdracht 05/scripts/code.js
+++ Labo 14 - Sem 2/Opdracht 05/scripts/code.js
@@ -1,7 +1,15 @@
-
 const setup = () => {
-        document.getElementById("knop").addEventListener("click", function() {
-            document.getElementById("txtOutput").innerHTML = "Welkom!";
-        });
+    let pElement = document.getElementById("knop");
+    pElement.addEventListener("click", wijzig);
 }
+
+const wijzig = () => {
+    let woord = document.getElementById("txtOutput");
+    if (woord.innerHTML === "Hello world!") {
+        woord.innerHTML = "Welkom";
+    } else {
+        woord.innerHTML = "Hello world!";
+    }
+}
+
 window.addEventListener("load", setup);
